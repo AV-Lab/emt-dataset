c4c72a2a2c20d64d24facb1deb87ad8f15aa8754
namadjidku <nadya.abdelmadjid@gmail.com>
2024-09-30 18:01:45 +0400

    Added plotting annotations

--- docs/js/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Select all div elements with the class 'md-copyright'
-    var copyrightDivs = document.querySelectorAll('.md-copyright');
-
-    // Loop through each selected div and remove its text content
-    copyrightDivs.forEach(function(div) {
-        div.innerHTML = ''; // Clear the content of each div
-    });
-});
